372b43310130b5f54428e774fa5bd8bed9cec8fb
Midry <115441271+midry3125@users.noreply.github.com>
2025-09-01 22:39:07 +0900

    ビルドの出力先を指定できるように

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
-go build -o dist/emeral.dll -buildmode=c-shared main.go
+@echo off
+
+set dest="dist/emeral.dll"
+
+if not "%~1"=="" set dest="%~1"
+
+go build -o "%dest%" -buildmode=c-shared main.go
